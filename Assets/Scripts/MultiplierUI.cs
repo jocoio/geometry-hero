0e614c2d8881935bff126b1ce2106204f7214d5e
Vincent Carlino <vjcarlino22@gmail.com>
2019-02-10 23:59:26 -0500

    Organizing files

---
+++ Assets/Scripts/MultiplierUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MultiplierUI : MonoBehaviour {
+
+	private Text text;
+	private Score score;
+
+	// Use this for initialization
+	void Start () {
+		text = gameObject.GetComponent<Text>();
+		score = GameObject.FindGameObjectWithTag("Score").GetComponent<Score>();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		text.text = score.multiplier.ToString() + "X";
+	}
+}
